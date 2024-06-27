b769a5b5cff1ec2f783570d0a31944977e834b48
faridaammi <farid.tiznit.1999@gmail.com>
2024-06-27 13:36:24 +0100

    update style checkbox

--- src/components/Input/CheckBox.vue
+++ src/components/Input/CheckBox.vue
@@ -1,3 +1,4 @@
+
 <script setup>
 import Checkbox from 'primevue/checkbox';
 
@@ -6,7 +7,7 @@ import Checkbox from 'primevue/checkbox';
     <Checkbox v-model="checked"  :pt="{
       box:{class : 'bg-white border dark:border-gray-600 chekbx shadow-sm dark:bg-gray-800 rounded-md text-gray-900 w-6 h-6 flex flex-shrink-0 justify-center items-center focus-within:border-primary'},
       input:{class : 'bg-error'},
-      icon:{class : ' bg-primary border text-white dark:border-gray-600 shadow-sm chekbx dark:bg-gray-800 rounded-md text-gray-900 w-6 h-6 flex flex-shrink-0 justify-center items-center focus-within:border-primary'}
+      icon:{class : ' bg-primary border text-white p-1 dark:border-gray-600 shadow-sm chekbx dark:bg-gray-800 rounded-md text-gray-900 w-6 h-6 flex flex-shrink-0 justify-center items-center focus-within:border-primary'}
     }" />
 
 
