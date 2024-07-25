7afd3ce9ed57672d9234ff0c3de79c2fa0a910d2
faridaammi <farid.tiznit.1999@gmail.com>
2024-07-25 15:26:47 +0100

    update inputColor

--- src/components/Input/InputColor.vue
+++ src/components/Input/InputColor.vue
@@ -7,7 +7,9 @@
             input:{class:'rounded-md'}
         }"/>
         </div>
-        <input v-model="model" 
+        <input v-model="formattedColor" 
+
+        
         class="bg-white border ps-5 border-gray-300 !h-10 pe-10 text-gray-900 text-sm shadow-sm rounded-lg focus:ring-primary focus:border-primary focus:ring-1 block w-full p-2.5  dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"  
         />
     </div>
@@ -16,21 +18,22 @@
 import ColorPicker from 'primevue/colorpicker';
 import { ref, watch } from 'vue';
 
-// defineOptions({
-//     inheritAttrs:false 
-// })
-const model = ref('#0d4aa1');
-const emit = defineEmits(['update:modelValue'])
-// const handleInput = () => {
-//     emit('update:modelValue', model.value);
-//     console.log(model.value);
-// };
-watch(model, (newVal) => {
-    if (!newVal.startsWith('#')) {
-        model.value = '#' + newVal;
-    }
+const props = defineProps(['inputModel']);
+const emit = defineEmits(['update:modelValue']);
+
+const model = ref(props.inputModel);
+
+const formattedColor = ref(`#${model.value}`);
+
+watch(model, (newValue) => {
+    const formatted = `#${newValue}`;
+    formattedColor.value = formatted;
+    emit('update:modelValue', formatted);
+});
+
+watch(() => props.inputModel, (newVal) => {
+    model.value = newVal.replace('#', '');
 });
 </script>
 
 
-<!-- :class="{ 'ps-10': start,'pe-10': end }"  -->
