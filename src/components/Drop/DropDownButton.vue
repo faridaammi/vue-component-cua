a93c582c4d31da98a7e84fccb5a403a43b02efc6
faridaammi <farid.tiznit.1999@gmail.com>
2024-10-02 11:50:55 +0100

    add new version in calender and dropDownButton

--- src/components/Drop/DropDownButton.vue
+++ src/components/Drop/DropDownButton.vue
@@ -5,7 +5,8 @@
       <span  @click="togglePanel">
         <slot name="drop"></slot>
       </span>
-      <div v-if="modelValue" id="dropdownHelper" class="z-10 flex items-center end-0 border mt-1 border-gray bg-white divide-y divide-gray-100 rounded-lg shadow  dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600 absolute " >
+      <div v-if="showDropdown" id="dropdownHelper" class=" " 
+      :class="['z-10 flex items-center border mt-1 border-gray bg-white divide-y divide-gray-100 rounded-lg shadow  dark:border-gray-600 dark:bg-gray-800 dark:divide-gray-600 absolute',getAlign(props.position)]">
         <slot name="panel"/>
       </div>
   </span>
@@ -13,29 +14,46 @@
 
 <script setup>
 
-import { defineProps, defineEmits } from 'vue';
-  
+import { defineProps, defineEmits, ref,watch } from 'vue';
+const showDropdown = ref(false);
+
   // Props and emits setup
   const props = defineProps({
     modelValue: {
       type: Boolean,
-      required: true
+      default: true
     },
-      isClickAway:{
+    isClickAway:{
     type:Boolean,
     default: true
- },
+    },
+    position:{
+    type:String,
+    default: 'end'
+    }
   });
    const onClickAway = (event)=>{
-      if(props.isClickAway === true)
+      if(props.isClickAway === true){
         emit('update:modelValue', false);
+        showDropdown.value = false;
+      }
 
-  // console.log("away");
  }
   const emit = defineEmits(['update:modelValue']);
-  
-  // Function to toggle panel visibility
-  const togglePanel = () => {
+  watch(() => props.modelValue, (newValue,oldValue)=>{
+    showDropdown.value = newValue;
+  })
+  const aligns = ref({
+    start : "start-0",
+    end : "end-0",
+    
+})
+const getAlign = (align) => {
+  return aligns.value[align] || aligns.value.end;
+}
+
+const togglePanel = () => {
+    showDropdown.value = !showDropdown.value;
     emit('update:modelValue', !props.modelValue);
   };
 
--- src/components/Input/DateInput.vue
+++ src/components/Input/DateInput.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div class="relative" >
-      <Calendar v-bind="$attrs" iconDisplay="input" v-model="model"
+      <Calendar v-bind="$attrs" type="text" iconDisplay="input" v-model="model" panelClass="!min-w-min"
+
       :pt="{
-        panel : {class : 'min-w-min w-10rem dark:bg-gray-800 dark:text-light dark:hover:bg-gray-800 dark:hover:text-gray-400'},
+        panel : {class : ' dark:bg-gray-800 dark:text-light dark:hover:bg-gray-800 dark:hover:text-gray-400'},
         root: { class : 'border-0 block w-ful shadow-none' },
         inputIcon :{ class:'text-error end-3 right-none'},	
         header :{ class:' dark:text-light dark:bg-gray-800'},	
@@ -16,16 +17,96 @@
 </template>
 
 <script setup>
+import { usePrimeVue } from "primevue/config";
+const primevue = usePrimeVue();
+
 const model = defineModel()
 import Calendar from 'primevue/calendar';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 defineProps(
   ['size']
 );
 defineOptions({
     inheritAttrs:false
 })
+onMounted(() => {
+  primevue.config.locale.dateAfter = "Après le",
+  primevue.config.locale.dateBefore = "Avant le",
+  primevue.config.locale.dateFormat = "dd/mm/yy",
+  primevue.config.locale.dateIs = "La date est",
+  primevue.config.locale.dateIsNot = "La date n'est pas",
+  primevue.config.locale.dayNames = [
+      "Dimanche",
+      "Lundi",
+      "Mardi",
+      "Mercredi",
+      "Jeudi",
+      "Vendredi",
+      "Samedi"
+    ],
+  primevue.config.locale.dayNamesMin =  [
+      "Dim",
+      "Lun",
+      "Mar",
+      "Mer",
+      "Jeu",
+      "Ven",
+      "Sam"
+    ];
+    primevue.config.locale.dayNamesShort = [
+      "Dim",
+      "Lundi",
+      "Mardi",
+      "Mercr",
+      "Jeudi",
+      "Vendr",
+      "Sam"
+    ],
+  primevue.config.locale.monthNames = [
+      "Janvier",
+      "Février",
+      "Mars",
+      "Avril",
+      "Mai",
+      "Juin",
+      "Juillet",
+      "Août",
+      "Septembre",
+      "Octobre",
+      "Novembre",
+      "Décembre"
+    ],
+  primevue.config.locale.monthNamesShort = [
+      "Janv",
+      "Févr",
+      "Mars",
+      "Avr",
+      "Mai",
+      "Juin",
+      "Juill",
+      "Août",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Déc"
+    ],
+    primevue.config.locale.nextDecade = "Décennie suivante",
+    primevue.config.locale.nextHour = "Heure suivante",
+    primevue.config.locale.nextMinute = "Minute suivante",
+    primevue.config.locale.nextMonth = "Mois suivant",
+    primevue.config.locale.nextSecond = "Seconde suivante",
+    primevue.config.locale.nextYear = "Année suivante",
+    primevue.config.locale.now = "Maintenant",
+    primevue.config.locale.prevDecade = "Décennie précédente",
+    primevue.config.locale.prevHour = "Heure précédente",
+    primevue.config.locale.prevMinute = "Minute précédente",
+    primevue.config.locale.prevMonth = "Mois précédent",
+    primevue.config.locale.prevSecond = "Seconde précédente",
+    primevue.config.locale.prevYear = "Année précédente",
+    primevue.config.locale.today = "Aujourd'hui";
 
+
+});
 const sizes = ref({
     xs : "text-xs !h-6 bg-white shadow-sm border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:ring-1 focus:border-primary block w-full p-2.5  dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary",
     sm : "text-sm !h-8 bg-white shadow-sm border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:ring-1 focus:border-primary block w-full p-2.5  dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary",
@@ -36,4 +117,10 @@ const sizes = ref({
 const getSize = (size) => {
   return sizes.value[size] || sizes.value.md;
 }
+
+
+
+
 </script>
+
+
